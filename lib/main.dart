c86b261d66ca197dab5ffcf24298b67aea887d6f
SrAlbuquerque <guilhermealbuquerque2802@gmail.com>
2021-04-07 23:58:11 -0300

    Subindo o inicio do projeto meals

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'screens/categories_screen.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Vamos Cozinhar?',
+      theme: ThemeData(
+        primarySwatch: Colors.pink,
+        accentColor: Colors.amber,
+        fontFamily: 'Raleway',
+        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        textTheme: ThemeData.light().textTheme.copyWith(
+              headline6: TextStyle(fontSize: 20, fontFamily: 'RobotoCondensed'),
+            ),
+      ),
+      home: CategoriesScreen(),
+    );
+  }
+}
---
+++ lib/screens/categories_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:meals/models/category.dart';
+import '../components/category_item.dart';
+import '../data/dummy_data.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Vamos Cozinhar!'),
+      ),
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: DUMMY_CATEGORIES.map((cat) => CategoryItem(cat)).toList(),
+      ),
+    );
+  }
+}
